d6ea5b390a8c92219825f521eab97d92ea0cd6a0
Yahweasel <git-rpo@yahweasel.com>
2021-08-26 15:49:20 -0400

    Initial import

---
+++ ennuicastr-fastpunct-daemon.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 Yahweasel
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+import json
+import os
+import socketserver
+
+from fastpunct import FastPunct
+
+# Do some pre-punctuation just to prime the pump
+fastpunct = FastPunct()
+fastpunct.punct([
+    "john smiths dog is creating a ruccus",
+    "ys jagan is the chief minister of andhra pradesh",
+    "we visted new york last year in may"
+])
+
+# Get the server socket
+addr = "/tmp/ennuicastr-fastpunct-daemon.sock"
+try:
+    os.unlink(addr)
+except OSError:
+    pass
+
+# Main server
+class FastPunctDaemonReqHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        while True:
+            # Expected format is one JSON object per line
+            line = self.rfile.readline()
+            if line == "":
+                break
+
+            try:
+                obj = json.loads(line)
+                # Expected format: {"c":"fastpunct","i":[array of strings]}
+                res = fastpunct.punct(obj["i"])
+                # Return format: {"o":[result]}
+                ress = json.dumps({
+                    "o": res
+                })
+                self.wfile.write(bytes(ress + "\n", "utf8"))
+            except Exception:
+                break
+
+class FastPunctDaemonServer(socketserver.ThreadingMixIn, socketserver.UnixStreamServer):
+    pass
+
+server = FastPunctDaemonServer(addr, FastPunctDaemonReqHandler)
+server.serve_forever()
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup
+
+setup(
+    name = "ennuicastr-fastpunct-daemon",
+    version = "0.0.1",
+    author = "Yahweasel",
+    author_email = "git-rpo@yahweasel.com",
+    license = "ISC",
+    install_requires = ["fastpunct"]
+)
